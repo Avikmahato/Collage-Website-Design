f032ba41ab0ee747af33e547dbb7b555d01f9b3c
Avik Mahato <128340287+Avikmahato@users.noreply.github.com>
2025-05-08 02:32:17 +0530

    Add files via upload

---
+++ contact/contact.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>BIST</title>
+  <link rel="stylesheet" href="contact.css">
+  <link rel="stylesheet" href="../footer/footer.css">
+</head>
+
+<body>
+  <?php
+    include("../nav.php");
+  ?>
+  <section id="contact" class="contact-section">
+    <div class="container">
+      <h1>Contact Us</h1>
+      <p>If you have any questions, feel free to reach out to our contact persons below:</p>
+
+      <div class="contact-persons">
+        <div class="contact-person">
+          <h2>Avijit Roy</h2>
+          <p><strong>Mobile:</strong><a href="tel: +91 9932667579">+91 9932667579</a></p>
+        </div>
+
+        <div class="contact-person">
+          <h2>Tamal Chatterjee</h2>
+          <p><strong>Mobile:</strong> <a href="tel:+91 9851177856">+91 9851177856</a></p>
+        </div>
+        <div class="contact-person">
+          <h2>Pridip Kumar Mahato</h2>
+          <p><strong>Mobile:</strong><a href="+91 9932711293">+91 9932711293</a> </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <?php
+
+  include("../footer/footer.php");
+  ?>
+</body>
+
+</html>
